977c59a80c01df63c4e14063aaa7fc4d45ae589e
Johan Tan <johan_tan@tech.gov.sg>
2022-06-06 19:12:39 +0800

    Initial commit

---
+++ day1/dov-bear/python/build-push-docker.sh
@@ -0,0 +1,2 @@
+docker build -t ghcr.io/johantannh/dov-bear:v1python .
+docker push ghcr.io/johantannh/dov-bear:v1python
---
+++ day1/dov-bear/python/run-docker.sh
@@ -0,0 +1,1 @@
+docker run --rm -p 8080:3000 --name dov-bear-python ghcr.io/johantannh/dov-bear:v1python
