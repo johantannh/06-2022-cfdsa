97e040653be3588939c4f28d110a20a5b216fdb5
Johan Tan <>
2022-06-07 17:07:33 +0800

    feat: day2 commit and summarized commands

---
+++ day2/workshop02/commands.sh
@@ -0,0 +1,28 @@
+exit 0
+
+# Create namespace "bggns"
+kubectl create namespace bggns
+
+# Apply DB deployment
+kubectl apply -f db-deploy.yaml -nbggns       
+
+# Check DB correctness
+kubectl exec <pod_name> -nbggns -it bash
+> mysql -u root -p
+
+# base64 of password to write into Secret. IMPORTANT: use -n to extract away the new line
+echo -n "changeit" | base64 
+
+# Apply backend app deployment
+kubectl apply -f backend-deploy.yaml
+
+# Get ConfigMap, Secret and Pods,LB etc with more details to check states
+kubectl get cm,secret,all -nbggns -owide
+
+# Example of viewing logs in a specified pod
+kubectl logs <pod_name> -nbggns
+# Delete DB deployment
+kubectl delete -f db-deploy.yaml -nbggns 
+
+# Delete backend app deployment 
+kubectl delete -f backend-deploy.yaml -nbggns
