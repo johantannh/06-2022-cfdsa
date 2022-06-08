543a5465eebec980da7e21ac85649d650c16c87c
Johan Tan <>
2022-06-08 17:03:52 +0800

    feat: day03 and some refactor of folders

---
+++ day3/day3-cmd-summary.sh
@@ -0,0 +1,39 @@
+exit 0
+
+# https://artifacthub.io/packages/helm/ingress-nginx/ingress-nginx
+# Install ingress-nginx
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo update
+
+# Check nginx template locally
+helm template my-ingress ingress-nginx/ingress-nginx -n ingress-nginx > ingress.yaml
+
+# Install ingress-nginx into cluster
+helm install ingress-nginx ingress-nginx/ingress-nginx -n ingress-nginx --create-namespace
+
+# Check what is running in cluster(after installing ingress-controller)
+kubectl get all -n ingress-nginx
+
+# Check what have I installed
+helm repo list
+
+# List what is installed
+helm ls -a
+
+# Uninstall an installed packaged item
+helm uninstall ingress-nginx -n ingress-nginx
+
+# Install Sealed secrets https://artifacthub.io/packages/helm/bitnami-labs/sealed-secrets
+helm repo add bitnami-labs https://bitnami-labs.github.io/sealed-secrets/
+helm repo update
+helm install sealed-secrets bitnami-labs/sealed-secrets -n sealed-secrets --create-namespace
+
+# Watch to see how the secrets are being created
+watch kubectl get all,cm,secret -n sealed-secrets
+# Worst case if anything happen, can just uninstall and reinstall again
+
+# Applying kustomization on client to see whats different
+kubectl apply -k . -oyaml --dry-run=client
+# kubectl apply -k . -oyaml --dry-run=server
+
+# Go to nip.io to get fake DNS for testing
